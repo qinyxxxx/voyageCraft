9ec5a96fc716d2b9cb70d80a8d9f4abace3e8d48
Yuxin Qin <qinyuxin97@gmail.com>
2025-01-09 21:57:53 -0800

    fix: path bug fix

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -7,7 +7,7 @@ migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object('config.Config')
+    app.config.from_object('backend.config.Config')
 
     db.init_app(app)
     migrate.init_app(app, db)
