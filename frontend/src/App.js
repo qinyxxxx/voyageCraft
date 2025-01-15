11a3a2430c41c691770aff71edba650056e7730f
Eileenyuan1 <69851001+Eileenyuan1@users.noreply.github.com>
2025-01-15 15:53:18 -0800

    Add Profile Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 import "./App.css"; 
+import Profile from "./js/Profile";
+import Login from "./js/Login";
+import Signup from "./js/Signup";
 import MainPage from "./js/MainPage";
 
 function App() {
   return (
-    <div className="App">
-      <MainPage />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </Router>
   );
 }
 
