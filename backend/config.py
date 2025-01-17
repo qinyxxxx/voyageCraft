4b91fc2f3840e12dc877531ff4652c746c460668
boooo <bopangyy@gmail.com>
2025-01-17 10:05:31 -0800

    config

--- backend/config.py
+++ backend/config.py
@@ -1,4 +1,4 @@
 class Config:
     # SECRET_KEY = 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:123456@localhost/voyage_craft'
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:231224us@localhost/voyage_craft'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
